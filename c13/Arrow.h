ea8de6b909cac72f8cce2996cfe0821a44b9e45d
martin <m@rtin.bz>
2022-01-23 14:26:05 -0300

    Working with arrow head

--- c13/Arrow.h
+++ c13/Arrow.h
@@ -14,13 +14,27 @@ namespace Shapes
 		public Graph_lib::Shape
 		  {
 			public:
-				Arrow (Graph_lib::Point p1, Graph_lib::Point p2, int arrow_head_length = 10);
+				Arrow (Graph_lib::Point p1, Graph_lib::Point p2);
+
+				void add (Graph_lib::Point p1, Graph_lib::Point p2);
+
+				static Graph_lib::Point find_arrow_length (Graph_lib::Point p1, Graph_lib::Point p2);
 
 			protected:
 				void draw_lines () const override;
 
 			private:
+				static double magnitude (Graph_lib::Point p1, Graph_lib::Point p2);
 
+				static double angle (Graph_lib::Point p1, Graph_lib::Point p2);
+
+				static Graph_lib::Point rotate (Graph_lib::Point p, int angle);
+
+				static Graph_lib::Point get_vector (Graph_lib::Point p1, Graph_lib::Point p2);
+
+				static double to_radian (double degree);
+
+				static double to_degree (double radian);
 		  };
   }
 #endif //_ARROW_H_
--- c13/e3.cpp
+++ c13/e3.cpp
@@ -10,16 +10,11 @@ try
   {
 	Simple_window win(Point {0, 0}, x_max(), y_max(), "Exercise 3");
 
-	Graph_lib::Rectangle box = {Point {100, 100}, Point {400, 200}};
-	box.set_color(Color::invisible);
-	box.set_fill_color(Color::white);
-
-	Shapes::Arrow a {Point {100, 100}, {400, 200}, 50};
-	a.set_style(Line_style(Line_style::dot, 2));
+	Shapes::Arrow a {Point {400, 400}, {200, 600}};
+	a.set_style(Line_style(Line_style::dot, 4));
 	a.set_color(Color::dark_blue);
-	a.move(100, 200);
+//	a.move(400, -200);
 
-	win.attach(box);
 	win.attach(a);
 	win.wait_for_button();
   }

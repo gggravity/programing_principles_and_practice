c371bc1658d8cdc9013f5d0613b44cbb6907c732
martin <m@rtin.bz>
2022-01-31 22:21:26 -0300

    Exercise 8

---
+++ c14/e8/build/_deps/catch2-build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/martin.LAPLAND/CLionProjects/programing-principles-and-practice/c14/e8/build/_deps/catch2-src/src
+# Build directory: C:/Users/martin.LAPLAND/CLionProjects/programing-principles-and-practice/c14/e8/build/_deps/catch2-build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
